b196973214f2b1f91951fd72358097edf4ffdbb8
kivia <vikas@posteo.de>
2021-01-19 16:15:45 +0100

    Add supuertokens for localhost:3567

--- blitz.config.js
+++ blitz.config.js
@@ -1,11 +1,28 @@
-const { sessionMiddleware, simpleRolesIsAuthorized } = require("@blitzjs/server")
+// const { sessionMiddleware, simpleRolesIsAuthorized } = require("@blitzjs/server")
+
+let supertokens = require("supertokens-node")
+let Session = require("supertokens-node/recipe/session")
+let EmailPassword = require("supertokens-node/recipe/emailpassword")
+
+console.log("Init super tokens")
+
+supertokens.init({
+  supertokens: {
+    connectionURI: "https://localhost:3567",
+  },
+  appInfo: {
+    appName: "Blitz meets supertokens",
+    apiDomain: "http://localhost:3000", // Example: "https://api.supertokens.io",
+    websiteDomain: "http://localhost:3000",
+  },
+  recipeList: [
+    EmailPassword.init(), // initializes signin / sign up features
+    Session.init(), // initializes session features
+  ],
+})
 
 module.exports = {
-  middleware: [
-    sessionMiddleware({
-      isAuthorized: simpleRolesIsAuthorized,
-    }),
-  ],
+  middleware: [supertokens.middleware(), supertokens.errorHandler()],
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
